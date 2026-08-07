f30bfe21b8a7c79beef41e63a89a9912834e8662
kota1127nakajima-bit <kota1127.nakajima@icloud.com>
2026-08-07 21:03:31 +0900

    BaseModelの追加、submit関数の書き換え

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form 
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel 
 
 app = FastAPI()
 
@@ -10,18 +11,21 @@ app.mount(
     name="static"
 )
 
+# 受け取るJSONのデータ型を追加します
+class EssayRequest(BaseModel):
+    topic: str
+    essay: str
+
 
 @app.get("/")
 def home():
     return FileResponse("templates/writing.html")
 
 
+# 受け取り方を「Form」から「JSON」に変更
 @app.post("/submit")
-def submit(
-    topic: str = Form(...),
-    essay: str = Form(...)
-):
-    print(topic)
-    print(essay)
+def submit(request: EssayRequest):
+    print(request.topic)
+    print(request.essay)
 
     return {"message": "received"}
